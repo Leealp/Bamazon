175423b9c667d2ceec5c7c3086df0ec98fef2c42
AlphaLy <lyalpha2401@gmail.com>
2019-06-02 14:19:18 -0500

    created a table to display all the items in the bamazon database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,67 +20,18 @@ connection.connect(function(err,answer){
 	if (err) throw err;
   console.log("connected");
 
+  displayTable();
+
 	});	
 
+var displayTable = function(){
+  connection.query("SELECT * FROM products", function(err,res){
+    for(var i = 0; i<res.length; i++){
+      console.log(res[i].item_id+" || "+res[i].product_name+" || "+res[i].department_name+" || "+res[i].price+" || "+res[i].stock_quantity+"\n");
+    }
+  })
 
-// function bamazonSearch() {
-//   inquirer
-//     .prompt({
-//       name: "first",
-//       type: "list",
-//       message: "Hi! Welcome to bamazon shoping. Your all you can do online shopsmart?\n Below is the list of all we have in stock\n please tell me the ID of the product you would like to buy",
-//       choices: [
-//         "1 diaper for $35",
-//         "2 painting for $99",
-//         "3 mower for $299",
-//         "4 tire for $89",
-//         "5 pant for $49",
-//         "6 skirt for $29",
-//         "exit\n" 
-//       ]
-//     })
-//     .then(function(answer){
-//     	switch(answer.first){
-//     		case "1 diaper for $35":
-//     		case "2 painting for $99":
-//     		case "3 mower for $299":
-//     		case "4 tire for $89":
-//     		case "5 pant for $49":
-//     		case "6 skirt for $29":
-//     		quantity();
-//     		break;
-
-//     		 case "exit":
-//         	connection.end();
-//        		break;
-//     	}
-    	
-//     });
-    
-// }
-
-// function quantity(){
-//   inquirer
-//   	.prompt({
-//   		name: "second",
-//   		type: "input",
-//   		message:"how many would you like to buy?",
-
-
-//   	})
-//   	.then(function(answer){
-//   		var query = "SELECT stock_quantity FROM bamazon WHERE = answer.first";
-//   		connection.query(query, { second: answer.second }, function(err, res) {
-//   			console.log(answer);
-//   			// if(answer.second > )
-//   		});
-
-
-
-
-//   	});
-
-// }
+}
 
 
 
